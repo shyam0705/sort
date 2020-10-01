7eeaac30c47f5b5c45dce92f95a50f8915d1b031
shyam <shyam07052002@gmail.com>
2020-10-01 22:01:32 +0530

    selectionUpdate

--- src/components/SelectionMainComponent.js
+++ src/components/SelectionMainComponent.js
@@ -4,16 +4,16 @@ import styled, { css } from 'styled-components';
 import { Button, Jumbotron } from 'reactstrap';
 import { say } from './helper/speech.js';
 import AlgoInfo from './helper/algoInfo.js';
-import { SelectionSortDesc }    from './helper/selectionDesc.js';
+import { SelectionSortDesc } from './helper/selectionDesc.js';
+import ControlPannel from './helper/ControlPannel.js';
 function Display({ comparingText, swapText, currentMin }) {
     return (
-        <Jumbotron style={{ height: "500px"}}>
+        <Jumbotron style={{ height: "500px", margin: "auto", marginTop: "10px"}}>
             <div className="container">
                 <div className="row row-header">
                     <div className="col-12 col-sm-6">
                         <h1>Algorithm</h1>
                         <hr></hr>
-                      
                         <p>{comparingText}</p>
                         <p>{swapText}</p>
                     </div>
@@ -32,7 +32,9 @@ class SelectionMain extends Component {
             swapIndex: null,
             sorted: null,
             comparingText: "",
-            swapText:""
+            swapText: "",
+            play: false,
+            voice:false
         }
     }
     componentDidMount() {
@@ -42,18 +44,33 @@ class SelectionMain extends Component {
             console.log(this.state.arr);
         })
     }
+    voiceControl = () => {
+        this.setState({ voice: !this.state.voice });
+    }
+    resetState = () => {
+        this.setState({ minIndex: null});
+        this.setState({ comparingIndex: null});
+        this.setState({ swapIndex: null});
+        this.setState({ sorted: null});
+        this.setState({ comparingText: ""});
+        this.setState({ swapText: ""});
+    }
+    repeat = () => {
+        this.resetState();
+        this.onPlay();
+    }
     timer(ms) {
         return new Promise(res => setTimeout(res, ms));
     }
-    selectionOnclick = async () => {
+    onPlay = async () => {
         let speak = (text) => say(text);
         const arr = this.state.arr;
         const n = arr.length;
         for (var i = 0; i < n - 1; i++) {
             await this.timer(1000);
             this.setState({ minIndex: i });
-            speak(`setting ${arr[i]} as current min element,we will compare it with remaining element`);
-            await this.timer(6000);
+            if (this.state.voice) { speak(`setting ${arr[i]} as current min element,we will compare it with remaining element`) };
+            this.state.voice?await this.timer(6000):await this.timer(1000);
             var min = i;
             for (var j = i + 1; j < n; j++) {
                 this.setState({ comparingIndex: j });
@@ -62,9 +79,9 @@ class SelectionMain extends Component {
                 if (arr[j] < arr[min]) {
                     
                     this.setState({ swapText: `${arr[j]} is smaller than current min so we will update current min to ${arr[j]}` })
-                    speak(`${arr[j]} smaller than current min so we will update current min`);
+                    if (this.state.voice) { speak(`${arr[j]} smaller than current min so we will update current min`) };
                     this.setState({ minIndex: j });
-                    await this.timer(5000);
+                   this.state.voice?await this.timer(5000):await this.timer(1000);
                     min = j;
                 }
                 this.setState({ comparingText: "" });
@@ -90,6 +107,9 @@ class SelectionMain extends Component {
         this.setState({ sorted: i + 1 });
         this.setState({ minIndex: null });
         this.setState({ comparingtext: "List is sorted" });
+    }
+    onPause = async () => {
+
     }
     render() {
         const Bars = styled.div`
@@ -122,14 +142,14 @@ class SelectionMain extends Component {
 `;
         const list = this.state.arr.map((size, i) => {
             return (
-                <Bars height={`${size}px`} width={`${50 / this.state.arr.length}%`} active={this.state.minIndex === i} swap={this.state.swapIndex === i} comparing={this.state.comparingIndex === i} sorted={this.state.sorted>i} key={i}>
+                <Bars height={`${size}px`} width={`${75 / this.state.arr.length}%`} active={this.state.minIndex === i} swap={this.state.swapIndex === i} comparing={this.state.comparingIndex === i} sorted={this.state.sorted>i} key={i}>
                     <p>{size}</p>
                 </Bars>
             );
         }); 
         return (
             <div className="container">
-                <Button onClick={() => this.selectionOnclick()} >Selection sort</Button>
+                <ControlPannel play={this.state.play} voice={this.state.voice} onPlay={() => this.onPlay()} onPause={() => this.onPause()} repeat={() => this.repeat()} voiceControl={() => this.voiceControl()} />
                 <div className="row">
                     <Container className="col-md-8 col-sm-12">
                         {list}
