89022623edd904d44caf3efd067e65aa931b13af
shyam <shyam07052002@gmail.com>
2020-10-04 16:43:35 +0530

    final

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import BubbleMain from './BubbleMainComponent';
 import SelectionMain from './SelectionMainComponent';
 import Header from './HeaderComponent.js';
